3c5add6d790968db78fa9e0b74102b98938bf8dd
linlianfu <1241352445@qq.com>
2018-09-08 21:18:12 +0800

    适配common改造

--- src/main/java/priv/llf/ability/course/south/dao/mongo/CourseMongoDao.java
+++ src/main/java/priv/llf/ability/course/south/dao/mongo/CourseMongoDao.java
@@ -1,13 +1,13 @@
 package priv.llf.ability.course.south.dao.mongo;
 
+import cn.eleven.common.dao.MDSpringDaoTemplate;
 import org.springframework.stereotype.Repository;
 import priv.llf.ability.course.south.model.mongo.CourseMongo;
-import priv.llf.commons.dao.MDSpringDaoTemplate;
 
 /**
- * @author: eleven
- * @date: 2018/8/11 14:15
- * @description:
+ * @author eleven
+ * @date 2018/8/11
+ * @description
  */
 @Repository
 public class CourseMongoDao extends MDSpringDaoTemplate<CourseMongo> {
